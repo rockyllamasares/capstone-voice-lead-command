ee576ce01f17083f5d1adfbe26504560acefcf01
Rocky llamasares <54110440+rockyllamasares@users.noreply.github.com>
2022-07-27 10:19:01 +0800

    capstone project

--- template/header.php
+++ template/header.php
@@ -21,7 +21,9 @@
     <!-- darkly -->
     <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="asset/css/darkly.css">
-
+    <!-- BOOTSTRAP CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
 
     <!-- getboostrap -->
     <!-- <link rel="stylesheet" href="asset/css1/css/bootstrap-grid.min.css">
